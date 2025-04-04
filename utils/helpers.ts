02b4d00f4308fbe57639d0e0b7f4820803e904e6
Dima Baikalov <dmbaikalov@Gmail.com>
2025-04-04 18:04:40 +0300

    reworked sumTimeInSeconds helpers func

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,14 +1,19 @@
-export function sumTimeInSeconds(time: string[]) {
-  let totalMinutes = 0;
+export function sumTimeInSeconds(times: string[]): string {
   let totalSeconds = 0;
 
-  time.forEach((t) => {
-    const [minutes, seconds] = t.split(':').map(Number);
-    totalMinutes += minutes;
-    totalSeconds += seconds;
-  });
+  for (const time of times) {
+    const [minutesStr, secondsStr] = time.split(':');
+    const minutes = parseInt(minutesStr, 10);
+    const seconds = parseInt(secondsStr, 10);
 
-  totalSeconds += totalMinutes * 60;
+    if (isNaN(minutes) || isNaN(seconds)) {
+      throw new Error(
+        `Invalid time format: "${time}". Expected format "mm:ss"`
+      );
+    }
 
-  return `${totalSeconds}`;
+    totalSeconds += minutes * 60 + seconds;
+  }
+
+  return totalSeconds.toString();
 }
