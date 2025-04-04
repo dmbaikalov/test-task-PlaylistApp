184f416acf23a0b8503b138f14053123a94113fb
Dima Baikalov <dmbaikalov@Gmail.com>
2025-04-04 18:13:40 +0300

    .

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,16 +1,10 @@
-export function sumTimeInSeconds(times: string[]): string {
+export function sumTimeInSeconds(time: string[]): string {
   let totalSeconds = 0;
 
-  for (const time of times) {
-    const [minutesStr, secondsStr] = time.split(':');
+  for (const t of time) {
+    const [minutesStr, secondsStr] = t.split(':');
     const minutes = parseInt(minutesStr, 10);
     const seconds = parseInt(secondsStr, 10);
-
-    if (isNaN(minutes) || isNaN(seconds)) {
-      throw new Error(
-        `Invalid time format: "${time}". Expected format "mm:ss"`
-      );
-    }
 
     totalSeconds += minutes * 60 + seconds;
   }
